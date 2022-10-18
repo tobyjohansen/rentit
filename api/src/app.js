106a2cb6c476cbd0f4a6f083f774fc2ba8e44da1
tobyjohansen <tobias.p.johansen@outlook.com>
2022-10-18 21:52:28 +0200

    Fixed cors, and fetch get cars

--- api/src/app.js
+++ api/src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const carRouter = require("./routes/carRoutes");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 //STINE PRØVER SEG PÅ ENDEPUNKTER
 app.get("/hello", (req, res) => res.send("Hello World!"));
--- client/rentit/src/components/carview/CarData.js
+++ client/rentit/src/components/carview/CarData.js
@@ -4,21 +4,21 @@ function CarData() {
   const [carData, setCarData] = useState([]);
 
   useEffect(() => {
-    fetch("127.0.0.1:3300/api/v1/cars")
-      .then((res) => res.json())
+    fetch("http://localhost:3300/api/v1/cars")
+      .then((res) => {
+        return res.json();
+      })
       .then((data) => {
         setCarData(data);
       });
   }, []);
 
+  const carJson = carData.data;
+
   return (
     <div>
       <h4>Cars</h4>
-      <ul>
-        {carData.map((data) => {
-          return <li>{carData.id}</li>;
-        })}
-      </ul>
+      <h1>{carData.status}</h1>
     </div>
   );
 }
