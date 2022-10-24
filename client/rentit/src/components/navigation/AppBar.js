06f2f3ccb8ce21c40f11cb4c5fbf08999d49d4ff
Stine Losnedahl <93385431+slosne@users.noreply.github.com>
2022-10-24 08:52:24 +0200

    Material UI import improvements in AppBar.js

--- client/rentit/src/components/navigation/AppBar.js
+++ client/rentit/src/components/navigation/AppBar.js
@@ -1,13 +1,15 @@
 import * as React from "react";
 import { styled, alpha } from "@mui/material/styles";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  InputBase,
+  MenuItem,
+  Menu,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
 import AccountCircle from "@mui/icons-material/AccountCircle";
