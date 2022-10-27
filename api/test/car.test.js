fc9a2458807abd44b7458cad2e3ac8593bb1bbb7
tobyjohansen <tobias.p.johansen@outlook.com>
2022-10-27 09:56:24 +0200

    Added testing and car creation tests

--- api/test/car.test.js
+++ api/test/car.test.js
@@ -1,3 +1,4 @@
+const { expect } = require("chai");
 const chai = require("chai");
 const assert = chai.assert;
 
@@ -22,10 +23,63 @@ const carObj1 = new Car(
 );
 
 describe("Testing Creation of Car object", () => {
-  describe("constructor", () => {
-    it("Constructor contains right value", () => {
-      const test = 1 + 2;
+  describe("Check for input validation", () => {
+    it("Testing validation for id", () => {
+      assert.throws(() => {
+        carObj1.checkFormValidation(null, "Toyota", 3500);
+      });
     });
-    it("Testing class creation", () => {});
+    it("Testing validation for model", () => {
+      assert.throws(() => {
+        carObj1.checkFormValidation(12, null, 3500);
+      });
+    });
+    it("Testing validation for price", () => {
+      assert.throws(() => {
+        carObj1.checkFormValidation(12, "Toyota", null);
+      });
+    });
+  });
+  describe("Testing Car Output", () => {
+    it("Testing if car output to object works", () => {
+      const car = {
+        id: 1,
+        price: 10,
+        model: "Miray",
+        brand: "Toyota",
+        year: 1998,
+        location: "Oslo",
+        availability: [1, 2, 4, 9],
+        type: "Personbil",
+        fuel: "Bensin",
+        gear: "Automat",
+        km_limit: 600,
+        extras: ["Hengerfeste"],
+        price_per_km_after_limit: 2,
+        owner: 8,
+      };
+
+      assert.deepEqual(carObj1.carObject, car);
+    });
+    it("Testing if car outputs to json", () => {
+      const car = {
+        id: 1,
+        price: 10,
+        model: "Miray",
+        brand: "Toyota",
+        year: 1998,
+        location: "Oslo",
+        availability: [1, 2, 4, 9],
+        type: "Personbil",
+        fuel: "Bensin",
+        gear: "Automat",
+        km_limit: 600,
+        extras: ["Hengerfeste"],
+        price_per_km_after_limit: 2,
+        owner: 8,
+      };
+
+      assert.deepEqual(carObj1.jsonStringify(), JSON.stringify(car));
+    });
   });
 });
