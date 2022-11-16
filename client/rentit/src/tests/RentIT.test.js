4750d661681a73f1e3f964208b9fcd8dec4bbdb2
Stine Losnedahl <93385431+slosne@users.noreply.github.com>
2022-11-16 18:57:26 +0100

    More integration tests

--- client/rentit/src/tests/RentIT.test.js
+++ client/rentit/src/tests/RentIT.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { getByLabelText, render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
 import AllCars from "../components/allCarsComponents/AllCars";
@@ -13,82 +13,125 @@ const MockAddCarForm = () => {
   );
 };
 
+describe("AddCarForm should show car data form for user to fill out", () => {
+  test("renders a textbox labeled 'Bilmerke'", () => {
+    render(<MockAddCarForm />);
+    const carBrand = screen.getByLabelText(/bilmerke/i);
+    expect(carBrand).toBeInTheDocument();
+  });
+  test("renders a textbox labeled 'Bilmodell'", () => {
+    render(<MockAddCarForm />);
+    const carModel = screen.getByLabelText(/Bilmodell/i);
+    expect(carModel).toBeInTheDocument();
+  });
+  test("renders a textbox labeled 'Pris/uke'", () => {
+    render(<MockAddCarForm />);
+    const carPrice = screen.getByLabelText(/Pris\/uke/i);
+    expect(carPrice).toBeInTheDocument();
+  });
+  test("renders a textbox labeled 'Årsmodell'", () => {
+    render(<MockAddCarForm />);
+    const carYear = screen.getByLabelText(/Årsmodell/i);
+    expect(carYear).toBeInTheDocument();
+  });
+  test("renders a textbox labeled 'Pris/km over grense'", () => {
+    render(<MockAddCarForm />);
+    const carPriceAfterLimit = screen.getByLabelText(/Pris\/km over grense/i);
+    expect(carPriceAfterLimit).toBeInTheDocument();
+  });
+  test("renders a textbox labeled 'Drivstoff'", () => {
+    render(<MockAddCarForm />);
+    const carFuel = screen.getByLabelText(/Drivstoff/i);
+    expect(carFuel).toBeInTheDocument();
+  });
+  test("renders a textbox labeled 'Girkasse'", () => {
+    render(<MockAddCarForm />);
+    const carGear = screen.getByLabelText(/Girkasse/i);
+    expect(carGear).toBeInTheDocument();
+  });
+  test("renders a textbox labeled 'Hentested'", () => {
+    render(<MockAddCarForm />);
+    const carLocation = screen.getByLabelText(/Hentested/i);
+    expect(carLocation).toBeInTheDocument();
+  });
+  test("renders a textbox labeled 'Registreringsnr'", () => {
+    render(<MockAddCarForm />);
+    const carRegNo = screen.getByLabelText(/Registreringsnr./i);
+    expect(carRegNo).toBeInTheDocument();
+  });
+  test("renders a textbox labeled 'Km-grense'", () => {
+    render(<MockAddCarForm />);
+    const carLimit = screen.getByLabelText(/Km-grense/i);
+    expect(carLimit).toBeInTheDocument();
+  });
+  test("renders a selectbox labeled 'Biltype'", () => {
+    render(<MockAddCarForm />);
+    const carType = screen.getByLabelText(/Biltype/i);
+    expect(carType).toBeInTheDocument();
+  });
+});
+
 describe("User should be able to register a new car to rent out", () => {
-  test("User should be able to add a car brand", () => {
+  test("User should be able to add and submit a car brand to AddCarForm", () => {
     render(<MockAddCarForm />);
     const carBrand = screen.getByRole("textbox", { name: /Bilmerke/i });
     user.type(carBrand, "Testmerke");
   });
-  test("User should be able to add a car model", () => {
+  test("User should be able to add and submit a car model to AddCarForm", () => {
     render(<MockAddCarForm />);
     const carModel = screen.getByRole("textbox", { name: /Bilmodell/i });
     user.type(carModel, "Testmodell");
   });
-  test("User should be able to add a car year", () => {
+  test("User should be able to add and submit a car year to AddCarForm", () => {
     render(<MockAddCarForm />);
     const carYear = screen.getByRole("textbox", { name: /Årsmodell/i });
     user.type(carYear, "TestÅrsmodell");
   });
-  test("User should be able to add car pickup location", () => {
+  test("User should be able to add and submit a car pickup location to AddCarForm", () => {
     render(<MockAddCarForm />);
     const carLocation = screen.getByRole("textbox", { name: /Hentested/i });
     user.type(carLocation, "TestHentested");
   });
-  test("User should be able to add wanted price per week", () => {
+  test("User should be able to add and submit wanted price per week to AddCarForm", () => {
     render(<MockAddCarForm />);
     const carPrice = screen.getByLabelText(/pris\/uke/i);
     user.type(carPrice, "3500 ");
   });
-  test("User should be able to add a max driving limit in km", () => {
+  test("User should be able to add and submit max driving limit in km to AddCarForm", () => {
     render(<MockAddCarForm />);
     const carLimit = screen.getByRole("textbox", { name: /Km-grense/i });
     user.type(carLimit, "TestKmGrense");
   });
-  test("User should be able to add car registration number", () => {
+  test("User should be able to add and submit car registration number to AddCarForm", () => {
     render(<MockAddCarForm />);
     const carRegNo = screen.getByRole("textbox", { name: /Registreringsnr./i });
     user.type(carRegNo, "TestRegNr");
   });
-  /*
-  test("User should be able to add car fuel type", () => {
+  test("User should be able to add and submit a car fuel type to AddCarForm", () => {
     render(<MockAddCarForm />);
-    const carFuel = screen.getByRole("checkbox", { name: /Drivstoff/i });
+    const carFuel = screen.getByRole("button", { name: /Drivstoff/i });
     user.click(carFuel);
   });
-  test("User should be able to add car gear type", () => {
+  test("User should be able to add a car gear type to AddCarForm", () => {
     render(<MockAddCarForm />);
-    const carGear = screen.getByRole("checkbox", { name: /Girtype/i });
+    const carGear = screen.getByRole("button", { name: /Girkasse/i });
     user.click(carGear);
   });
-  test("User should be able to add car type", () => {
+  test("User should be able to add a car type to AddCarForm", () => {
     render(<MockAddCarForm />);
-    const carType = screen.getByRole("checkbox", { name: /Biltype/i });
+    const carType = screen.getByRole("button", { name: /Biltype/i });
     user.click(carType);
-  });*/
-  test("User should be able to add a price per km if you exceed set driving limit", () => {
+  });
+  test("User should be able to add and submit a price per km if you exceed set driving limit to AddCarForm", () => {
     render(<MockAddCarForm />);
     const carPriceAfterLimit = screen.getByRole("textbox", {
       name: /pris\/km over grense/i,
     });
     user.type(carPriceAfterLimit, "10");
   });
-  /*
-  test("User should be able to add extra equipment included in the rent, if any", () => {
+  /*test("All added items to add car form should be submitted to backend when you click 'Lagre' button", () => {
     render(<MockAddCarForm />);
-    const carEquipment = screen.getByRole("checkbox", {
-      name: /Ekstrautstyr/i,
-    });
-    user.click(carEquipment);
-  });
-  test("User should be able to choose what week numbers car is available for rent", () => {
-    render(<MockAddCarForm />);
-    const availableWeeks = screen.getByRole("checkbox", {
-      name: /Ledige uker/i,
-    });
-    user.click(availableWeeks);
-  });
-  test("All added items to add car form should be submitted to backend when you click 'Lagre' button", () => {
-    render(<MockAddCarForm />);
+
   });*/
 });
 
