0be11c60362a2345a64c0ae80c5d62f70278e5f7
Stine Losnedahl <93385431+slosne@users.noreply.github.com>
2022-11-21 13:36:14 +0100

    Small display text changes

--- client/rentit/src/components/myCarComponents/EditCarForm.js
+++ client/rentit/src/components/myCarComponents/EditCarForm.js
@@ -111,7 +111,7 @@ export default function EditCarForm() {
       price_per_km_after_limit: enteredPricePerKmOver,
     };
 
-    //Added Update fetch method for updateing to api
+    //Added Update fetch method for updating to api
     const url = "http://localhost:3300/api/v1/cars/" + enteredRegNumber;
     fetch(url, {
       method: "PATCH",
